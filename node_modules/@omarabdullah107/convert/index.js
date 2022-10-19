080ffdfb200b1f5322aa87682f958312ef7c680e
omarabdullah107 <mromar502@hotmail.com>
2022-10-20 00:24:48 +0200

    updating some data

--- node_modules/@omarabdullah107/convert/index.js
+++ node_modules/@omarabdullah107/convert/index.js
@@ -1,18 +1,5 @@
-function convertcm(x){
-    result = x * 100;
-    return result;
-
-}
-
-
-function convertm(x){
-    result=x / 100;
-    return result;
-}
-
-
-module.exports = convertcm;
-module.exports = convertm;
+const convercmTom = (number) => number/100
+module.exports = convercmTom; 
 
 
 
