a9f5f7b69ce0acb5a5e46745359d2a4926019372
omarabdullah107 <mromar502@hotmail.com>
2022-10-20 00:17:56 +0200

    pushing new updates

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const convertm = require("@omarabdullah107/convert");
-//the bmi function is basically the weight over the sqaure root of the height
-
-function bmi(weight,height){
-    sqheight = Math.pow(height,2) // the height sqaured in cm 
-    bmi = weight / convertm(sqheight); // using the predefind function (convertm) to convert the cm height into meter
-    return bmi*100  // multiplying the bmi to make it in kg/m**2
-}
-
-
+const convercmTom = require("@omarabdullah107/convert");
+const bmi = (weight,height) => (weight/convercmTom(Math.pow(height,2)))*100
 module.exports = bmi ;
